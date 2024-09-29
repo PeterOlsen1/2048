6ba87830024f30e897f784ce7cc73fe4f5c0f653
Peter Olsen <134348337+PeterOlsen1@users.noreply.github.com>
2024-09-28 21:38:33 -0500

    Add files via upload

---
+++ src/lib/components/Box.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let text = "hello";
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+
+    .box {
+        background-color: rgba(255, 169, 138, 0.658);
+        width: 75px;
+        height: 75px;
+        text-align: center;
+        align-items: center;
+        border-radius: 10px;
+        color: white;
+        font-family: "Open Sans", sans-serif;
+        font-weight: 600;
+        display: flex;
+        justify-content: center;
+        font-size: 18pt;
+        
+    }
+</style>
+
+<div class="box">
+    <p>{text}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Box from "$lib/components/Box.svelte";
+</script>
+
+<style>
+    .board {
+        background-color: rgba(200, 200, 200, 0.2);
+        border-radius: 10px;
+        padding: 20px;
+        gap: 10px;
+        width: 245px;
+        height: 245px;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr;
+    }
+
+    .board-container {
+        width: 100vw;
+        height: 100vw;
+        background-color: rgb(76, 76, 76);
+        display: flex;
+        justify-content: center;
+        padding: 30px;
+    }
+</style>
+<div class="board-container">
+    <div class="board">
+        <Box text=1></Box>
+        <!-- <Box text=2></Box>
+        <Box text=3></Box>
+        <Box text=4></Box>
+        <Box text=5></Box>
+        <Box text=6></Box>
+        <Box text=7></Box>
+        <Box text=8></Box>
+        <Box text=9></Box> -->
+    </div>
+</div>
